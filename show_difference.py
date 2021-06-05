5877cc597b8d02343899651dbfbc323fb51bac32
ZiMeng Huang <LukeHuang@sjtu.edu.cn>
2021-06-05 21:24:53 +0800

    Add tools for showing difference between different versions.

---
+++ show_difference.py
@@ -0,0 +1,27 @@
+import os
+
+total_results = os.listdir('mid_result')
+
+total_res = {}
+for i in total_results:
+    if i[0:5] != 'final':
+        continue
+    cur_path = "mid_result/" + i
+    print(cur_path)
+    with open(cur_path,"r",encoding='utf-8') as f:
+        for line in f.readlines():
+            split_content = line.split(' ')
+            # print(split_content)
+            cur_paper_id = int(split_content[0])
+            cur_pred_res = int(split_content[1][:-1])
+            if cur_paper_id not in total_res:
+                cur_dict = {cur_pred_res: 1}
+                total_res[cur_paper_id] = cur_dict
+            else:
+                cur_dict = total_res[cur_paper_id]
+                if cur_pred_res not in cur_dict:
+                    cur_dict[cur_pred_res] = 1
+                else:
+                    cur_dict[cur_pred_res] += 1
+
+print(total_res)
