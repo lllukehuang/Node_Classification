0a0e954965bd58da17cedfeda87202c434bb2df4
YifanWang00 <357021685@qq.com>
2021-06-06 22:22:09 +0800

    update

--- stellagraph/load_data_as_heteroGraph_v2.py
+++ stellagraph/load_data_as_heteroGraph_v2.py
@@ -82,7 +82,7 @@ author2, author3 = find_coauthor(paper_author_info)
 
 
 square_edges = {
-    "p2p": pd.DataFrame({"source": ori_paper, "target":ref_paper}),
+    # "p2p": pd.DataFrame({"source": ori_paper, "target":ref_paper}),
     "a2p": pd.DataFrame({"source": author1, "target":paper1})
     # "a2a": pd.DataFrame({"source": author2, "target":author3})
 }
