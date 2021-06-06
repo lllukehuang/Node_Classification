b2799ea9eeb98b5eb3e129b65eb320b21f6d209d
YifanWang00 <357021685@qq.com>
2021-06-07 00:44:58 +0800

    Finish loading

--- stellagraph/load_data_as_heteroGraph.py
+++ stellagraph/load_data_as_heteroGraph.py
@@ -49,61 +49,66 @@ def find_coauthor(df:DataFrame):
         # print('---------------------------------\n')
 
 
-### Load Raw Data ###
-paper_author_info = pd.read_csv("./labeled_papers_with_authors.csv")
-paper_reference_info = pd.read_csv("./paper_reference.csv")
-# Pre-processing
-print("Processing data...")
-paper_reference_info = paper_reference_info[(paper_reference_info['paper_id']<4844) & (paper_reference_info['reference_id']<4844)].reset_index()
 
-paper_author_info['author_id'] = paper_author_info['author_id'].apply(lambda x:'a' + str(int(x)))
-paper_author_info['paper_id'] = paper_author_info['paper_id'].apply(lambda x:'p' + str(int(x)))
+def constructHeteroGraph():
 
-paper_reference_info['paper_id'] = paper_reference_info['paper_id'].apply(lambda x:'p' + str(int(x)))
-paper_reference_info['reference_id'] = paper_reference_info['reference_id'].apply(lambda x:'p' + str(int(x)))
-# print(paper_author_info)
-# print(paper_reference_info)
+    ### Load Raw Data ###
+    paper_author_info = pd.read_csv("./labeled_papers_with_authors.csv")
+    paper_reference_info = pd.read_csv("./paper_reference.csv")
+    # Pre-processing
+    print("Processing data...")
+    paper_reference_info = paper_reference_info[(paper_reference_info['paper_id']<4844) & (paper_reference_info['reference_id']<4844)].reset_index()
 
-### Edges Construction ###
-print("Constructing edges...")
-source = []
-target = []
+    paper_author_info['author_id'] = paper_author_info['author_id'].apply(lambda x:'a' + str(int(x)))
+    paper_author_info['paper_id'] = paper_author_info['paper_id'].apply(lambda x:'p' + str(int(x)))
 
-# paper -> paper (one_way)
-ori_paper = paper_reference_info['paper_id'].tolist()
-ref_paper = paper_reference_info['reference_id'].tolist()
-source = source + ori_paper
-target = target + ref_paper
-# author -> paper (one-way)
-author1 = paper_author_info['author_id'].tolist()
-paper1 = paper_author_info['paper_id'].tolist()
-source = source + author1
-target = target + paper1
-# author <-> author (two-way)
-author2, author3 = find_coauthor(paper_author_info)
-source = source + author2
-target = target + author3
+    paper_reference_info['paper_id'] = paper_reference_info['paper_id'].apply(lambda x:'p' + str(int(x)))
+    paper_reference_info['reference_id'] = paper_reference_info['reference_id'].apply(lambda x:'p' + str(int(x)))
+    # print(paper_author_info)
+    # print(paper_reference_info)
 
+    ### Edges Construction ###
+    print("Constructing edges...")
+    source = []
+    target = []
 
-square_edges = pd.DataFrame({"source":source, "target":target})
-# print(square_edges)
-
-### Nodes Construction ###
-print("Constructing nodes...")
-# paper nodes (with labels)
-paper_node_info = pd.DataFrame({"paper_id":paper_author_info['paper_id'], "label":paper_author_info['label']}).drop_duplicates().reset_index()
-square_paper = pd.DataFrame({"conference":paper_node_info['label']}, index=paper_node_info['paper_id'])
-
-# author nodes (no labels)
-author_node_info = pd.DataFrame({"author_id":paper_author_info['author_id']}).drop_duplicates().reset_index()
-square_author = pd.DataFrame(index=author_node_info['author_id'])
+    # paper -> paper (one_way)
+    ori_paper = paper_reference_info['paper_id'].tolist()
+    ref_paper = paper_reference_info['reference_id'].tolist()
+    source = source + ori_paper
+    target = target + ref_paper
+    # author -> paper (one-way)
+    author1 = paper_author_info['author_id'].tolist()
+    paper1 = paper_author_info['paper_id'].tolist()
+    source = source + author1
+    target = target + paper1
+    # author <-> author (two-way)
+    author2, author3 = find_coauthor(paper_author_info)
+    source = source + author2
+    target = target + author3
 
 
-### Merge ###
-print("Constructing graph...")
-square_paper_and_author = StellarDiGraph(
-    {"paper":square_paper, "author":square_author},
-    square_edges)
-print('==================================')
-print(square_paper_and_author.info())
-print('==================================')
+    square_edges = pd.DataFrame({"source":source, "target":target})
+    # print(square_edges)
+
+    ### Nodes Construction ###
+    print("Constructing nodes...")
+    # paper nodes (with labels)
+    paper_node_info = pd.DataFrame({"paper_id":paper_author_info['paper_id'], "label":paper_author_info['label']}).drop_duplicates().reset_index()
+    square_paper = pd.DataFrame({"conference":paper_node_info['label']}, index=paper_node_info['paper_id'])
+
+    # author nodes (no labels)
+    author_node_info = pd.DataFrame({"author_id":paper_author_info['author_id']}).drop_duplicates().reset_index()
+    square_author = pd.DataFrame(index=author_node_info['author_id'])
+
+
+    ### Merge ###
+    print("Constructing graph...")
+    square_paper_and_author = StellarDiGraph(
+        {"paper":square_paper, "author":square_author},
+        square_edges)
+    print('==================================')
+    print(square_paper_and_author.info())
+    print('==================================')
+
+    return square_paper_and_author
